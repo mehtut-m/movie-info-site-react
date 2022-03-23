fecdfff08d4ac29bcee026f374b853defca8e379
mehtut-m <mehtut.m@gmail.com>
2022-03-24 00:48:04 +0700

    config route

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import Swiper from 'swiper';
+
 import './App.scss';
+import Header from './components/header/Header';
+import Footer from './components/footer/Footer';
+import RouteConfig from './routes/RouteConfig';
 
 function App() {
-  return <div>App</div>;
+  return (
+    <>
+      <Header />
+      <RouteConfig />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
